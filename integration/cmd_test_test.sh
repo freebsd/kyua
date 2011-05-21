6d469816662f1cd61873353f47e484760fb115c6
Julio Merino <jmmv@google.com>
2011-05-21 20:05:02 +0000

    Implement integration tests for the 'test' command
    Most of the new tests are a copy/paste from the tests of 'list', with the
    appropriate adjustments to make them work for 'test'.
    
    Some of these tests are marked as expected failures due to some shortcomings
    in the current implementation of 'test'.  These will be addressed separately.
    
    Also, it's hard to decide where to draw the line in what to test here. So far
    I have kept the tests restricted to the behavior of the 'test' command, but
    we probably want to go further and ensure that the runtime engine behaves
    correctly when running any kind of test programs (isolation, etc.).  Maybe
    these belong somewhere else though...
    
    This change was r124 in Subversion.

--- integration/cmd_test_test.sh
+++ integration/cmd_test_test.sh
@@ -170,50 +170,645 @@ EOF
 }
 
 
-utils_test_case subdirs
-subdirs_body() {
+utils_test_case no_args
+no_args_body() {
     cat >Kyuafile <<EOF
 syntax("kyuafile", 1)
 test_suite("integration")
-atf_test_program{name="top"}
-include("dir/CustomKyuafile")
+atf_test_program{name="simple_all_pass"}
+include("subdir/Kyuafile")
 EOF
+    utils_cp_helper metadata .
+    utils_cp_helper simple_all_pass .
 
-    mkdir dir
-    cat >dir/CustomKyuafile <<EOF
+    mkdir subdir
+    cat >subdir/Kyuafile <<EOF
 syntax("kyuafile", 1)
-test_suite("integration")
-atf_test_program{name="nested"}
+test_suite("integration2")
+atf_test_program{name="simple_some_fail"}
 EOF
+    utils_cp_helper simple_some_fail subdir
 
     cat >expout <<EOF
-top:pass  ->  passed
-top:skip  ->  skipped: The reason for skipping is this
-dir/nested:fail  ->  failed: This fails on purpose
-dir/nested:pass  ->  passed
+simple_all_pass:pass  ->  passed
+simple_all_pass:skip  ->  skipped: The reason for skipping is this
+subdir/simple_some_fail:fail  ->  failed: This fails on purpose
+subdir/simple_some_fail:pass  ->  passed
+
+3/4 passed (1 failed)
+EOF
+    atf_check -s exit:1 -o file:expout -e empty kyua test
+}
+
+
+utils_test_case one_arg__subdir
+one_arg__subdir_body() {
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("top-level")
+include("subdir/Kyuafile")
+EOF
+
+    mkdir subdir
+    cat >subdir/Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("in-subdir")
+atf_test_program{name="simple_all_pass"}
+EOF
+    utils_cp_helper simple_all_pass subdir
+
+    cat >expout <<EOF
+subdir/simple_all_pass:pass  ->  passed
+subdir/simple_all_pass:skip  ->  skipped: The reason for skipping is this
+
+2/2 passed (0 failed)
+EOF
+    atf_check -s exit:0 -o file:expout -e empty kyua test subdir
+}
+
+
+utils_test_case one_arg__test_case
+one_arg__test_case_body() {
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("top-level")
+atf_test_program{name="first"}
+atf_test_program{name="second"}
+EOF
+    utils_cp_helper simple_all_pass first
+    utils_cp_helper simple_all_pass second
+
+    cat >expout <<EOF
+first:skip  ->  skipped: The reason for skipping is this
+
+1/1 passed (0 failed)
+EOF
+    atf_check -s exit:0 -o file:expout -e empty kyua test first:skip
+}
+
+
+utils_test_case one_arg__test_program
+one_arg__test_program_body() {
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("top-level")
+atf_test_program{name="first"}
+atf_test_program{name="second"}
+EOF
+    utils_cp_helper simple_all_pass first
+    utils_cp_helper simple_some_fail second
+
+    cat >expout <<EOF
+second:fail  ->  failed: This fails on purpose
+second:pass  ->  passed
+
+1/2 passed (1 failed)
+EOF
+    atf_check -s exit:1 -o file:expout -e empty kyua test second
+}
+
+
+utils_test_case one_arg__invalid
+one_arg__invalid_body() {
+cat >experr <<EOF
+kyua: E: Test case component in 'foo:' is empty.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test foo:
+
+cat >experr <<EOF
+kyua: E: Program name '/a/b' must be relative to the test suite, not absolute.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test /a/b
+}
+
+
+utils_test_case many_args__ok
+many_args__ok_body() {
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("top-level")
+include("subdir/Kyuafile")
+atf_test_program{name="first"}
+EOF
+    utils_cp_helper simple_all_pass first
+
+    mkdir subdir
+    cat >subdir/Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("in-subdir")
+atf_test_program{name="second"}
+EOF
+    utils_cp_helper simple_some_fail subdir/second
+
+    cat >expout <<EOF
+subdir/second:fail  ->  failed: This fails on purpose
+subdir/second:pass  ->  passed
+first:pass  ->  passed
+
+2/3 passed (1 failed)
+EOF
+    atf_check -s exit:1 -o file:expout -e empty kyua test subdir first:pass
+}
+
+
+utils_test_case many_args__invalid
+many_args__invalid_body() {
+cat >experr <<EOF
+kyua: E: Program name component in ':badbad' is empty.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test this-is-ok :badbad
+
+cat >experr <<EOF
+kyua: E: Program name '/foo' must be relative to the test suite, not absolute.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test this-is-ok /foo
+}
+
+
+utils_test_case many_args__no_match__all
+many_args__no_match__all_body() {
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("top-level")
+atf_test_program{name="first"}
+atf_test_program{name="second"}
+EOF
+    utils_cp_helper simple_all_pass first
+    utils_cp_helper simple_all_pass second
+
+    cat >experr <<EOF
+kyua: W: No test cases matched by the filter 'first1'.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test first1
+}
+
+
+utils_test_case many_args__no_match__some
+many_args__no_match__some_body() {
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("top-level")
+atf_test_program{name="first"}
+atf_test_program{name="second"}
+atf_test_program{name="third"}
+EOF
+    utils_cp_helper simple_all_pass first
+    utils_cp_helper simple_all_pass second
+    utils_cp_helper simple_some_fail third
+
+    cat >expout <<EOF
+first:pass  ->  passed
+first:skip  ->  skipped: The reason for skipping is this
+third:fail  ->  failed: This fails on purpose
+third:pass  ->  passed
 
 3/4 passed (1 failed)
 EOF
 
-    utils_cp_helper simple_all_pass top
-    utils_cp_helper simple_some_fail dir/nested
+    cat >experr <<EOF
+kyua: W: No test cases matched by the filter 'fifth'.
+kyua: W: No test cases matched by the filter 'fourth'.
+EOF
+    atf_check -s exit:1 -o file:expout -e file:experr kyua test first fourth \
+        third fifth
+}
+
+
+utils_test_case args_are_relative
+args_are_relative_body() {
+    mkdir root
+    cat >root/Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration-1")
+atf_test_program{name="first"}
+atf_test_program{name="second"}
+include("subdir/Kyuafile")
+EOF
+    utils_cp_helper simple_all_pass root/first
+    utils_cp_helper simple_some_fail root/second
+
+    mkdir root/subdir
+    cat >root/subdir/Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration-2")
+atf_test_program{name="third"}
+atf_test_program{name="fourth"}
+EOF
+    utils_cp_helper simple_all_pass root/subdir/third
+    utils_cp_helper simple_some_fail root/subdir/fourth
+
+    cat >expout <<EOF
+first:pass  ->  passed
+first:skip  ->  skipped: The reason for skipping is this
+subdir/fourth:fail  ->  failed: This fails on purpose
+
+2/3 passed (1 failed)
+EOF
+    atf_check -s exit:1 -o file:expout -e empty kyua test \
+        -k "$(pwd)/root/Kyuafile" first subdir/fourth:fail
+}
+
+
+utils_test_case only_load_used_test_programs
+only_load_used_test_programs_body() {
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration")
+atf_test_program{name="first"}
+atf_test_program{name="second"}
+EOF
+    utils_cp_helper simple_all_pass first
+    utils_cp_helper bad_test_program second
+
+    cat >expout <<EOF
+first:pass  ->  passed
+first:skip  ->  skipped: The reason for skipping is this
+
+2/2 passed (0 failed)
+EOF
+    CREATE_COOKIE="$(pwd)/cookie"; export CREATE_COOKIE
+    atf_check -s exit:0 -o file:expout -e empty kyua test first
+    if test -f "${CREATE_COOKIE}"; then
+        atf_fail "An unmatched test case has been executed, which harms" \
+            "performance"
+    fi
+}
+
+
+utils_test_case missing_test_program
+missing_test_program_body() {
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration")
+atf_test_program{name="first"}
+EOF
+    utils_cp_helper simple_all_pass first
+    utils_cp_helper simple_all_pass second
+
+    cat >experr <<EOF
+kyua: W: No test cases matched by the filter 'second'.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test second
+}
+
+
+utils_test_case missing_test_case
+missing_test_case_body() {
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration")
+atf_test_program{name="first"}
+EOF
+    utils_cp_helper simple_all_pass first
+
+    cat >experr <<EOF
+kyua: W: No test cases matched by the filter 'first:foobar'.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test first:foobar
+}
+
+
+utils_test_case missing_kyuafile__no_args
+missing_kyuafile__no_args_body() {
+    cat >experr <<EOF
+kyua: E: Load of 'Kyuafile' failed: File 'Kyuafile' not found.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test
+}
+
+
+utils_test_case missing_kyuafile__test_program
+missing_kyuafile__test_program_body() {
+    mkdir subdir
+    cat >subdir/Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration")
+atf_test_program{name="unused"}
+EOF
+    utils_cp_helper simple_all_pass subdir/unused
+
+    cat >experr <<EOF
+kyua: E: Load of 'Kyuafile' failed: File 'Kyuafile' not found.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test subdir/unused
+}
+
+
+utils_test_case missing_kyuafile__subdir
+missing_kyuafile__subdir_body() {
+    mkdir subdir
+    cat >subdir/Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration")
+atf_test_program{name="unused"}
+EOF
+    utils_cp_helper simple_all_pass subdir/unused
+
+    cat >experr <<EOF
+kyua: E: Load of 'Kyuafile' failed: File 'Kyuafile' not found.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test subdir
+}
+
+
+utils_test_case bogus_config
+bogus_config_body() {
+    cat >"${HOME}/.kyuarc" <<EOF
+Hello, world.
+EOF
+
+    cat >experr <<EOF
+kyua: E: Load of '${HOME}/.kyuarc' failed: Failed to load Lua file '${HOME}/.kyuarc': ${HOME}/.kyuarc:2: '<name>' expected near '<eof>'.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test
+}
+
+
+utils_test_case bogus_kyuafile
+bogus_kyuafile_body() {
+    cat >Kyuafile <<EOF
+Hello, world.
+EOF
+
+    cat >experr <<EOF
+kyua: E: Load of 'Kyuafile' failed: Failed to load Lua file 'Kyuafile': Kyuafile:2: '<name>' expected near '<eof>'.
+EOF
+    atf_check -s exit:1 -o empty -e file:experr kyua test
+}
+
+
+utils_test_case bogus_test_program
+bogus_test_program_body() {
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration")
+atf_test_program{name="bad_test_program"}
+EOF
+    utils_cp_helper bad_test_program .
+
+    cat >expout <<EOF
+bad_test_program:__test_program__  ->  broken: Failed to load list of test cases: bad_test_program: Invalid header for test case list; expecting Content-Type for application/X-atf-tp version 1, got 'This is not a valid test program!'
+
+0/1 passed (1 failed)
+EOF
     atf_check -s exit:1 -o file:expout -e empty kyua test
+}
+
+
+utils_test_case config__behavior
+config__behavior_body() {
+    cat >"${HOME}/.kyuarc" <<EOF
+syntax("config", 1)
+test_suite_var("suite1", "X-the-variable", "value1")
+test_suite_var("suite2", "X-the-variable", "value2")
+EOF
+
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+atf_test_program{name="config1", test_suite="suite1"}
+atf_test_program{name="config2", test_suite="suite2"}
+atf_test_program{name="config3", test_suite="suite3"}
+EOF
+    utils_cp_helper config config1
+    utils_cp_helper config config2
+    utils_cp_helper config config3
+
+    atf_check -s exit:1 -o save:stdout -e empty kyua test
+    atf_check -s exit:0 -o ignore -e empty \
+        grep 'config1:get_variable.*failed' stdout
+    atf_check -s exit:0 -o ignore -e empty \
+        grep 'config2:get_variable.*passed' stdout
+    atf_check -s exit:0 -o ignore -e empty \
+        grep 'config3:get_variable.*skipped' stdout
+}
+
+
+utils_test_case config_flag__default_system
+config_flag__default_system_body() {
+    cat >kyua.conf <<EOF
+syntax("config", 1)
+test_suite_var("integration", "X-the-variable", "value2")
+EOF
+
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration")
+atf_test_program{name="config"}
+EOF
+    utils_cp_helper config .
+
+    atf_check -s exit:0 -o match:"get_variable.*skipped" -e empty kyua test
+    export KYUA_SYSCONFDIR="$(pwd)"
+    atf_expect_fail "Cannot mock the location of the configuration directory"
+    atf_check -s exit:0 -o match:"get_variable.*passed" -e empty kyua test
+}
+
+
+utils_test_case config_flag__default_home
+config_flag__default_home_body() {
+    cat >kyuarc <<EOF
+syntax("config", 1)
+test_suite_var("integration", "X-the-variable", "value2")
+EOF
+
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration")
+atf_test_program{name="config"}
+EOF
+    utils_cp_helper config .
+
+    atf_check -s exit:0 -o match:"get_variable.*skipped" -e empty kyua test
+    mv kyuarc "${HOME}/.kyuarc"
+    atf_check -s exit:0 -o match:"get_variable.*passed" -e empty kyua test
+}
+
+
+utils_test_case config_flag__explicit
+config_flag__explicit_body() {
+    cat >kyuarc <<EOF
+syntax("config", 1)
+test_suite_var("integration", "X-the-variable", "value2")
+EOF
+
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration")
+atf_test_program{name="config"}
+EOF
+    utils_cp_helper config .
+
+    atf_check -s exit:0 -o match:"get_variable.*skipped" -e empty kyua test
+    atf_check -s exit:0 -o match:"get_variable.*passed" -e empty kyua test \
+        -c kyuarc
+    atf_check -s exit:0 -o match:"get_variable.*passed" -e empty kyua test \
+        --config=kyuarc
+}
+
+
+utils_test_case variable_flag__no_config
+variable_flag__no_config_body() {
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+atf_test_program{name="config1", test_suite="suite1"}
+atf_test_program{name="config2", test_suite="suite2"}
+atf_test_program{name="config3", test_suite="suite3"}
+EOF
+    utils_cp_helper config config1
+    utils_cp_helper config config2
+    utils_cp_helper config config3
+
+    check_stdout() {
+        atf_check -s exit:0 -o ignore -e empty \
+            grep 'config1:get_variable.*failed' stdout
+        atf_check -s exit:0 -o ignore -e empty \
+            grep 'config2:get_variable.*passed' stdout
+        atf_check -s exit:0 -o ignore -e empty \
+            grep 'config3:get_variable.*skipped' stdout
+    }
+
+    atf_expect_fail "--variable not implemented"
+
+    atf_check -s exit:1 -o save:stdout -e empty kyua test \
+        -v "suite1.X-the-variable=value1" \
+        -v "suite2.X-the-variable=value2"
+    check_stdout
+
+    atf_check -s exit:1 -o save:stdout -e empty kyua test \
+        --variable="suite1.X-the-variable=value1" \
+        --variable="suite2.X-the-variable=value2"
+    check_stdout
+}
+
+
+utils_test_case variable_flag__override_config
+variable_flag__override_config_body() {
+    cat >"${HOME}/.kyuarc" <<EOF
+syntax("config", 1)
+test_suite_var("suite1", "X-the-variable", "value1")
+test_suite_var("suite2", "X-the-variable", "should not be used")
+EOF
+
+    cat >Kyuafile <<EOF
+syntax("kyuafile", 1)
+atf_test_program{name="config1", test_suite="suite1"}
+atf_test_program{name="config2", test_suite="suite2"}
+atf_test_program{name="config3", test_suite="suite3"}
+EOF
+    utils_cp_helper config config1
+    utils_cp_helper config config2
+    utils_cp_helper config config3
+
+    check_stdout() {
+        atf_check -s exit:0 -o ignore -e empty \
+            grep 'config1:get_variable.*failed' stdout
+        atf_check -s exit:0 -o ignore -e empty \
+            grep 'config2:get_variable.*passed' stdout
+        atf_check -s exit:0 -o ignore -e empty \
+            grep 'config3:get_variable.*skipped' stdout
+    }
+
+    atf_expect_fail "--variable not implemented"
+
+    atf_check -s exit:1 -o save:stdout -e empty kyua test \
+        -v "suite2.X-the-variable=value2"
+    check_stdout
+
+    atf_check -s exit:1 -o save:stdout -e empty kyua test \
+        --variable="suite2.X-the-variable=value2"
+    check_stdout
+}
+
+
+utils_test_case kyuafile_flag__no_args
+kyuafile_flag__no_args_body() {
+    cat >Kyuafile <<EOF
+This file is bogus but it is not loaded.
+EOF
+
+    cat >myfile <<EOF
+syntax("kyuafile", 1)
+test_suite("integration")
+atf_test_program{name="sometest"}
+EOF
+    utils_cp_helper simple_all_pass sometest
+
+    cat >expout <<EOF
+sometest:pass  ->  passed
+sometest:skip  ->  skipped: The reason for skipping is this
+
+2/2 passed (0 failed)
+EOF
+    atf_check -s exit:0 -o file:expout -e empty kyua test -k myfile
+    atf_check -s exit:0 -o file:expout -e empty kyua test --kyuafile=myfile
+}
+
+
+utils_test_case kyuafile_flag__some_args
+kyuafile_flag__some_args_body() {
+    cat >Kyuafile <<EOF
+This file is bogus but it is not loaded.
+EOF
+
+    cat >myfile <<EOF
+syntax("kyuafile", 1)
+test_suite("hello-world")
+atf_test_program{name="sometest"}
+EOF
+    utils_cp_helper simple_all_pass sometest
+
+    cat >expout <<EOF
+sometest:pass  ->  passed
+sometest:skip  ->  skipped: The reason for skipping is this
+
+2/2 passed (0 failed)
+EOF
+    atf_check -s exit:0 -o file:expout -e empty kyua test -k myfile sometest
+    atf_check -s exit:0 -o file:expout -e empty kyua test --kyuafile=myfile \
+        sometest
 }
 
 
 atf_init_test_cases() {
     atf_add_test_case one_test_program__all_pass
     atf_add_test_case one_test_program__some_fail
-
     atf_add_test_case many_test_programs__all_pass
     atf_add_test_case many_test_programs__some_fail
-
     atf_add_test_case expect__all_pass
     atf_add_test_case expect__some_fail
 
-    atf_add_test_case subdirs
+    atf_add_test_case no_args
+    atf_add_test_case one_arg__subdir
+    atf_add_test_case one_arg__test_case
+    atf_add_test_case one_arg__test_program
+    atf_add_test_case one_arg__invalid
+    atf_add_test_case many_args__ok
+    atf_add_test_case many_args__invalid
+    atf_add_test_case many_args__no_match__all
+    atf_add_test_case many_args__no_match__some
 
-    # TODO(jmmv): Add test cases for:
-    # - Failures: this requires code changes to pretty-print the error messages.
-    # - Flags: both -k and -c, in long and short versions.
+    atf_add_test_case args_are_relative
+
+    atf_add_test_case only_load_used_test_programs
+
+    atf_add_test_case config__behavior
+    atf_add_test_case config_flag__default_system
+    atf_add_test_case config_flag__default_home
+    atf_add_test_case config_flag__explicit
+    atf_add_test_case variable_flag__no_config
+    atf_add_test_case variable_flag__override_config
+
+    atf_add_test_case kyuafile_flag__no_args
+    atf_add_test_case kyuafile_flag__some_args
+
+    atf_add_test_case missing_test_program
+    atf_add_test_case missing_test_case
+
+    atf_add_test_case missing_kyuafile__no_args
+    atf_add_test_case missing_kyuafile__test_program
+    atf_add_test_case missing_kyuafile__subdir
+
+    atf_add_test_case bogus_config
+    atf_add_test_case bogus_kyuafile
+    atf_add_test_case bogus_test_program
 }
