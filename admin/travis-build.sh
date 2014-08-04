1df13ad18fe6cc09204e2429d4dfad8e23eb21a9
Julio Merino <jmmv@meroh.net>
2014-08-03 19:52:41 -0700

    Nuke the Kyua database across module builds
    Make sure the results of the kyua-testers build do not interfere with
    the results of the kyua-cli build.  When the schema of the database
    has been upgraded (as is the case now, with still-unreleased versions
    of Kyua), a run of kyua-testers with the old kyua binary will result
    in a database that requires manual migration, breaking the automatic
    tests of kyua-cli.

--- admin/travis-build.sh
+++ admin/travis-build.sh
@@ -44,6 +44,13 @@ for module in kyua-testers kyua-cli; do
     f="${f} LDFLAGS='-L/usr/local/lib -Wl,-R/usr/local/lib'"
     f="${f} PKG_CONFIG_PATH='/usr/local/lib/pkgconfig'"
 
+    # Because we are running the builds of various modules within the same
+    # environment, it's possible for the results of the kyua-testers build
+    # to interfere with the results of kyua-cli (e.g. when the database
+    # schema is updated and a manual db-migrate is required).
+    # For simplicity reasons, just nuke everything.
+    sudo rm -rf ~/.kyua /root/.kyua
+
     if [ "${AS_ROOT:-no}" = yes ]; then
         sudo make distcheck DISTCHECK_CONFIGURE_FLAGS="${f}"
     else
